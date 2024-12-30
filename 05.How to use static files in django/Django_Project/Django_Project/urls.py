67b17d1f466f0f0bc810889c7e98c7329b81545b
Web Developer <128967723+Ayyappan19@users.noreply.github.com>
2024-12-30 12:07:19 +0530

    Add files via upload

---
+++ 05.How to use static files in django/Django_Project/Django_Project/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from My_Application import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+
+
+      path('',views.home),
+
+]
